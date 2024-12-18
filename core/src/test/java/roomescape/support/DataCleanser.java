eeb1c8d9354f23ee697a16438ab231d95b4fa84b
haero77 <shineecard5@gmail.com>
2024-12-18 21:03:06 +0900

    test: 테스트간 유저 데이터 클렌징 과정 추가

--- core/src/test/java/roomescape/support/DataCleanser.java
+++ core/src/test/java/roomescape/support/DataCleanser.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import roomescape.domain.reservation.infra.ReservationJdbcRepository;
 import roomescape.domain.reservationtime.infra.ReservationTimeJdbcRepository;
 import roomescape.domain.theme.infra.ThemeJdbcRepository;
+import roomescape.domain.user.infra.UserJdbcRepository;
 
 @Component
 public class DataCleanser {
@@ -18,7 +19,11 @@ public class DataCleanser {
     @Autowired
     ThemeJdbcRepository themeRepository;
 
+    @Autowired
+    UserJdbcRepository userRepository;
+
     public void clean() {
+        userRepository.deleteAllInBatch();
         reservationRepository.deleteAllInBatch();
         reservationTimeRepository.deleteAllInBatch();
         themeRepository.deleteAllInBatch();
