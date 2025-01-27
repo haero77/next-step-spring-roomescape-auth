5d9180b0de5827200d812f86d1fcb39fb6c21e48
haero77 <shineecard5@gmail.com>
2025-01-27 20:04:20 +0900

    feat: 특정 테마의 특정 일자에 대한 예약 가능 시간대 조회 API 구현

--- core/src/main/java/roomescape/domain/reservationtime/api/ReservationTimeApi.java
+++ core/src/main/java/roomescape/domain/reservationtime/api/ReservationTimeApi.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.*;
 import roomescape.domain.reservationtime.application.ReservationTimeAppendRequest;
 import roomescape.domain.reservationtime.application.ReservationTimeCommandService;
 import roomescape.domain.reservationtime.application.ReservationTimeQueryService;
+import roomescape.domain.reservationtime.application.response.AvailableReservationTimeResponse;
 import roomescape.domain.reservationtime.domain.ReservationTime;
 import roomescape.domain.reservationtime.domain.ReservationTimeId;
 import roomescape.global.rest.ApiResponse;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -38,5 +40,16 @@ public class ReservationTimeApi {
     ) {
         timeCommandService.delete(new ReservationTimeId(timeId));
         return ApiResponse.okWithEmptyData();
+    }
+
+    @GetMapping("/api/times/available")
+    public ApiResponse<List<AvailableReservationTimeResponse>> getAvailableReservationTimes(
+            @RequestParam LocalDate date,
+            @RequestParam Long themeId
+    ) {
+        List<AvailableReservationTimeResponse> availableTimes =
+                timeCommandService.getAvailableReservationTimes(date, themeId);
+
+        return ApiResponse.ok(availableTimes);
     }
 }
--- core/src/main/java/roomescape/domain/reservationtime/application/ReservationTimeCommandService.java
+++ core/src/main/java/roomescape/domain/reservationtime/application/ReservationTimeCommandService.java
@@ -4,16 +4,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import roomescape.domain.common.ClockHolder;
 import roomescape.domain.reservation.domain.Reservation;
+import roomescape.domain.reservation.domain.ReservationDate;
 import roomescape.domain.reservation.domain.ReservationRepository;
+import roomescape.domain.reservationtime.application.response.AvailableReservationTimeResponse;
 import roomescape.domain.reservationtime.domain.ReservationTime;
 import roomescape.domain.reservationtime.domain.ReservationTimeId;
 import roomescape.domain.reservationtime.domain.ReservationTimeRepository;
 import roomescape.domain.reservationtime.exception.DupliactedReservationTimeException;
 import roomescape.domain.reservationtime.exception.ReservationTimeAlreadyInUse;
+import roomescape.domain.theme.domain.ThemeId;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +28,35 @@ public class ReservationTimeCommandService {
     private final ClockHolder clockHolder;
     private final ReservationTimeRepository timeRepository;
     private final ReservationRepository reservationRepository;
+
+    /**
+     * 특정 테마의 특정 일자에 대한 예약 가능한 시간 목록을 반환한다.
+     *
+     * @param date    예약 가능한 시간 목록을 조회할 날짜
+     * @param themeId 예약 가능한 시간 목록을 조회할 테마 식별자
+     * @return 예약 가능한 시간 목록
+     */
+    public List<AvailableReservationTimeResponse> getAvailableReservationTimes(
+            LocalDate date,
+            Long themeId
+    ) {
+        List<ReservationTime> allTimes = timeRepository.findAll();
+        List<Reservation> reservations = reservationRepository.findAllByThemeIdAndDate(
+                new ThemeId(themeId),
+                new ReservationDate(date)
+        );
+
+        Set<ReservationTimeId> reservedTimeIds = reservations.stream()
+                .map(Reservation::getTimeId)
+                .collect(Collectors.toSet());
+
+        // 예약되지 않은 시간들(=이용 가능한 시간)을 추출
+        List<ReservationTime> availableTimes = allTimes.stream()
+                .filter(time -> !reservedTimeIds.contains(time.getId()))
+                .toList();
+
+        return AvailableReservationTimeResponse.from(availableTimes);
+    }
 
     public ReservationTime append(final ReservationTimeAppendRequest request) {
         verifyUniqueStartAt(request.getStartAt());
