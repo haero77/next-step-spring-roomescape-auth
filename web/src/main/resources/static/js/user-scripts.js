5e6616a4f100a697bbee8afe21c772100e413b5b
haero77 <shineecard5@gmail.com>
2025-01-29 17:06:57 +0900

    view: 회원가입 폼 - 역할 선택 드롭다운 추가

--- web/src/main/resources/static/js/user-scripts.js
+++ web/src/main/resources/static/js/user-scripts.js
@@ -9,7 +9,7 @@ document.getElementById('logout-btn').addEventListener('click', function (event)
     credentials: 'include' // 쿠키를 포함시키기 위해 필요
   })
       .then(response => {
-        if(response.ok) {
+        if (response.ok) {
           // 로그아웃 성공, 페이지 새로고침 또는 리다이렉트
           window.location.reload();
         } else {
@@ -21,6 +21,58 @@ document.getElementById('logout-btn').addEventListener('click', function (event)
         console.error('Error:', error);
       });
 });
+
+/**
+ * 회원가입 버튼 클릭
+ */
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('signup-form');
+  form.addEventListener('submit', function(event) {
+    signUp(event);
+  });
+});
+
+function signUp(event) {
+  event.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const name = document.getElementById('name').value;
+  const role = document.getElementById('role').value;
+
+  if (!email || !password || !name || !role) {
+    alert('Please fill in all fields.');
+    return;
+  }
+
+  const signUpRequestBody = {
+    email: email,
+    password: password,
+    name: name,
+    role: role
+  };
+
+  fetch('/api/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(signUpRequestBody)
+  })
+      .then(response => {
+        if (!response.ok) {
+          alert('Signup request failed');
+          throw new Error('Signup request failed');
+        }
+
+        console.log('Signup successful:');
+        window.location.href = '/login';
+      })
+      .catch(error => {
+        console.error('Error during signup:', error);
+      });
+}
+
 
 function updateUIBasedOnLogin() {
   fetch('/login/check') // 로그인 상태 확인 API 호출
@@ -91,54 +143,6 @@ function login() {
 function signup() {
   // Redirect to signup page
   window.location.href = '/signup';
-}
-
-function register(event) {
-  // 폼 데이터 수집
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  const name = document.getElementById('name').value;
-
-  // 입력 필드 검증
-  if (!email || !password || !name) {
-    alert('Please fill in all fields.');
-    return; // 필수 입력 필드가 비어있으면 여기서 함수 실행을 중단
-  }
-
-  // 요청 데이터 포맷팅
-  const formData = {
-    email: email,
-    password: password,
-    name: name
-  };
-
-  // AJAX 요청 생성 및 전송
-  fetch('/members', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(formData)
-  })
-      .then(response => {
-        if (!response.ok) {
-          alert('Signup request failed');
-          throw new Error('Signup request failed');
-        }
-        return response.json(); // 여기서 응답을 JSON 형태로 변환
-      })
-      .then(data => {
-        // 성공적인 응답 처리
-        console.log('Signup successful:', data);
-        window.location.href = '/login';
-      })
-      .catch(error => {
-        // 에러 처리
-        console.error('Error during signup:', error);
-      });
-
-  // 폼 제출에 의한 페이지 리로드 방지
-  event.preventDefault();
 }
 
 function base64DecodeUnicode(str) {
